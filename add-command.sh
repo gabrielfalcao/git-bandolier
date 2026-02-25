b59b79118a2e9f314cc9c6e1771e9e0964935256
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-25 01:47:15 +0000

    saves state of work project "workbench" at 01:47:15 UTC

--- add-command.sh
+++ add-command.sh
@@ -71,8 +71,8 @@ for to_name in ${commands[@]}; do
     # echo copy "${from_dir}" "${to_dir}"
     cp -rf "${from_dir}" "${to_dir}"
     for rsfile in $(grep -l -E -r "(${from_name}|${from_title})" "${to_dir}"); do
-        sed -E "s/\b${from_name}\b/${to_name}/g" -i "${rsfile}"
-        sed -E "s/\b${from_title}\b/${to_title}/g" -i "${rsfile}"
+        sed -E "s/${from_name}/${to_name}/g" -i "${rsfile}"
+        sed -E "s/${from_title}/${to_title}/g" -i "${rsfile}"
     done
     cargo check
 
@@ -94,7 +94,7 @@ pub use ${to_name}::{
 
 " >> ./src/cli/commands/mod.rs
 
-    sed -E "s,^(\s*)[/][/](\s*)((Command::)?\b${to_title}\b),\1\2\3,g" -i src/main.rs
+    sed -E "s,^(\s*)[/][/](\s*)((Command::)?${to_title}),\1\2\3,g" -i src/main.rs
 
 
     if ! cargo run -- "${to_name}" --help; then

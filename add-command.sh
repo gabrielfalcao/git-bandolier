c7e9bae70c79fb7b4f19e0367d72dbab6ad1ef31
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-25 01:44:21 +0000

    saves add-command.sh

--- add-command.sh
+++ add-command.sh
@@ -12,7 +12,6 @@ declare -a commands=(
     "context"
     "switch"
     "path"
-    "sh"
     "goto"
     "list"
     "init"
@@ -61,6 +60,10 @@ echo -en "\x1b[2J\x1b[3J\x1b[H"
 for to_name in ${commands[@]}; do
     to_title=$(heck-string --to=pascal "${to_name}")
     to_dir="${commands_dir}/${to_name}"
+
+    echo -e  "\x1b[1;38;5;154madding command ${to_name}\x1b[0m"
+
+
     if [ -e "${to_dir}" ]; then
         rm -rf "${to_dir}"
     fi
@@ -68,8 +71,8 @@ for to_name in ${commands[@]}; do
     # echo copy "${from_dir}" "${to_dir}"
     cp -rf "${from_dir}" "${to_dir}"
     for rsfile in $(grep -l -E -r "(${from_name}|${from_title})" "${to_dir}"); do
-        sed -E "s/${from_name}/${to_name}/g" -i "${rsfile}"
-        sed -E "s/${from_title}/${to_title}/g" -i "${rsfile}"
+        sed -E "s/\b${from_name}\b/${to_name}/g" -i "${rsfile}"
+        sed -E "s/\b${from_title}\b/${to_title}/g" -i "${rsfile}"
     done
 
     echo "
@@ -90,7 +93,7 @@ pub use ${to_name}::{
 
 " >> ./src/cli/commands/mod.rs
 
-    sed -E "s,^(\s*)[/][/](\s*)((Command::)?${to_title}),\1\2\3,g" -i src/main.rs
+    sed -E "s,^(\s*)[/][/](\s*)((Command::)?\b${to_title}\b),\1\2\3,g" -i src/main.rs
 
     if ! cargo run -- "${to_name}" --help; then
         echo -e  "\x1b[1;38;5;196mFAILED\x1b[0m"

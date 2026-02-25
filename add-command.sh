080d151cd47462cf8d208e6cc54077eba0623ddc
Gabriel Falcão <gabrielteratos@gmail.com>
2026-02-25 01:23:08 +0000

    add-command.sh

--- add-command.sh
+++ add-command.sh
@@ -7,7 +7,7 @@ set -o pipefail
 unset IFS
 export IFS=$'\n'
 
-declare -a subcommands=(
+declare -a commands=(
     "env"
     "context"
     "switch"
@@ -44,28 +44,59 @@ declare -- title=""
 declare -- target_dir=""
 declare -- target_path=""
 declare -- mod_rsx=""
-declare -- subcommands_dir="src/cli/subcommands"
+declare -- commands_dir="src/cli/commands"
 
 declare -- from_name="bootstrap"
 declare -- from_title=$(heck-string --to=pascal "${from_name}")
-declare -- from_dir="${subcommands_dir}/${from_name}"
+declare -- from_dir="${commands_dir}/${from_name}"
 
 declare -- to_name=""
 declare -- to_title=""
 declare -- to_dir=""
+declare -- rsfile=""
 
 declare -p ${!from_*}
 
-for to_name in ${subcommands[@]}; do
+echo -en "\x1b[2J\x1b[3J\x1b[H"
+for to_name in ${commands[@]}; do
     to_title=$(heck-string --to=pascal "${to_name}")
-    to_dir="${subcommands_dir}/${to_name}"
+    to_dir="${commands_dir}/${to_name}"
     if [ -e "${to_dir}" ]; then
         rm -rf "${to_dir}"
     fi
 
+    # echo copy "${from_dir}" "${to_dir}"
     cp -rf "${from_dir}" "${to_dir}"
-    refactors "${from_name}" "${from_name}" -wp "${to_dir}"
-    refactors "${from_title}" "${from_title}" -wp "${to_dir}"
+    for rsfile in $(grep -l -E -r "(${from_name}|${from_title})" "${to_dir}"); do
+        sed -E "s/${from_name}/${to_name}/g" -i "${rsfile}"
+        sed -E "s/${from_title}/${to_title}/g" -i "${rsfile}"
+    done
+
+    echo "
+pub use commands::${to_name}::{
+    ${to_title}Command, ${to_title}SharedOpt, ${to_title}DirOpt, ${to_title}FileOpt,
+    ${to_title}Opt,
+};
+
+" >> ./src/cli/mod.rs
+
+    echo "
+
+pub mod ${to_name};
+pub use ${to_name}::{
+    ${to_title}Command, ${to_title}SharedOpt, ${to_title}DirOpt, ${to_title}FileOpt,
+    ${to_title}Opt,
+};
+
+" >> ./src/cli/commands/mod.rs
+
+    sed -E "s,^(\s*)[/][/](\s*)((Command::)?${to_title}),\1\2\3,g" -i src/main.rs
+
+    if ! cargo run -- "${to_name}" --help; then
+        echo -e  "\x1b[1;38;5;196mFAILED\x1b[0m"
+        break
+    fi
+
     git add -f "${to_dir}"
     git diff HEAD -- "${to_dir}"
     git commit "${to_dir}" -m "add boilerplate subcommand ${to_name}"

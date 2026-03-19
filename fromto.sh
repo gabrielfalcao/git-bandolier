0b14c3ebfba206b025b511b73c0463eae6ffce96
Gabriel Falcão <gabrielteratos@gmail.com>
2026-03-19 20:26:22 +0000

    saves fromto.sh

--- fromto.sh
+++ fromto.sh
@@ -1,37 +1,140 @@
+#!/usr/bin/env bash
+
+set -umeTE
+set +f
+set -o pipefail
+unset IFS
+export IFS=$'\n'
+
+declare -- script_name="$(basename "${BASH_SOURCE[0]}")"
+declare -- script_path="$(2>/dev/random 1>/dev/random cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+declare -- this_script_path="${script_path}/${script_name}"
+
 declare -a from_commands=(
-"write"
-"init"
-"remove"
-"goto"
-"context"
-"bootstrap"
-"enqueue"
-"update"
-"delete"
-"today"
-"refresh"
-"shell"
-"web"
-"path"
-"parse"
-"doctor"
-"read"
-"show"
-"server"
-"env"
-"edit"
-"load"
-"add"
-"stash"
-"list"
-"task"
-"switch"
-"find"
-"export"
-"absorb"
-"thatday"
-"import"
-"save"
-"client"
-"tool"
+    "absorb"
+    "add"
+    "bootstrap"
+    "client"
+    "context"
+    "delete"
+    "doctor"
+    "edit"
+    "enqueue"
+    "env"
+    "export"
+    "find"
+    "goto"
+    "import"
+    "init"
+    "list"
+    "load"
+    "parse"
+    "path"
+    "read"
+    "refresh"
+    "remove"
+    "save"
+    "server"
+    "shell"
+    "show"
+    "stash"
+    "switch"
+    "task"
+    "thatday"
+    "today"
+    "tool"
+    "update"
+    "web"
+    "write"
 )
+declare -a git_bandolier_commands=(
+    "st"
+    "json"
+    "br"
+    "remotes"
+
+
+
+)
+
+on_exit() {
+    set +x
+}
+on_ctrlc() {
+    1>&2 echo -e "\x1b[1;38;2;253;67;83m\rAborted with Ctrl-C\x1b[0m"
+    exit 130
+}
+trap on_exit exit
+trap on_ctrlc hup
+trap on_ctrlc int
+trap on_ctrlc bus
+trap on_ctrlc segv
+trap on_ctrlc sys
+
+declare -a argv=(${@})
+declare -i argc=${#argv[@]}
+
+declare -i code=0
+
+declare -i current=0
+declare -- arg=""
+declare -- pos=""
+
+declare -i index=0
+declare -- arg=""
+declare -- param=""
+
+declare -i next_index=0
+declare -- next_arg=""
+declare -- next_param=""
+
+declare -i skip_next=0
+
+declare -i lineno=0
+declare -i line_number=0
+declare -- line=""
+
+declare -- argument=""
+declare -- field=""
+declare -- key=""
+declare -- name=""
+declare -- path=""
+declare -- value=""
+
+# <GIT>
+declare -- git_repo_path=""
+if ! git_repo_path=$(2>${stderr} git rev-parse --show-toplevel); then
+    code=$?
+fi
+# </GIT>
+export IFS=$'\n'
+
+main() {
+
+    if [ ${argc} -eq 0 ]; then
+        1>&2 echo -e "[$(basename "${BASH_SOURCE[0]}") error]" "missing arguments"
+        exit 1
+    fi
+
+    for index in ${!argv[@]}; do
+        current=$(($index + 1))
+        arg="${argv[$index]}"
+        pos="$(printf '%*s of %s' ${#argc} ${current} ${argc})"
+
+        case "${arg}" in
+            -h | --help)
+                1>&2 echo -e "HELP"
+                ;;
+            *)
+                1>&2 echo -e "[$(basename "${BASH_SOURCE[0]}") argument ${pos}]" "${arg@Q}"
+                ;;
+        esac
+    done
+
+}
+
+if [ "${0}" == "${BASH_SOURCE[0]}" ]; then
+    main
+else
+    1>&2 echo -e "${BASH_SOURCE[0]} appears to being used as a library by ${0@Q}"
+fi

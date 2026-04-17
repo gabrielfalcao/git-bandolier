8a3f3de7fc49d1a39f4b569f3c8774d2e3286ee1
Gabriel Falcão <gabrielteratos@gmail.com>
2026-04-17 07:40:56 +0000

    src/cli/commands/path/opts.rs

---
+++ src/cli/commands/path/opts.rs
@@ -0,0 +1,45 @@
+use clap::Args;
+use iocore::Path;
+
+use crate::cli::commands::switch::shared::PathSharedOpt;
+use crate::dispatch::ArgsDispatcher;
+use crate::{Error, Result};
+
+#[derive(Args, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PathDirOpt {
+    #[clap(flatten)]
+    opt: PathSharedOpt,
+}
+
+impl PathDirOpt {
+    pub fn path(&self) -> Path {
+        self.opt.path()
+    }
+}
+
+impl ArgsDispatcher<Error> for PathDirOpt {
+    fn dispatch(&self) -> Result<()> {
+        let path = self.path();
+        println!("path: {path}");
+        Ok(())
+    }
+}
+
+#[derive(Args, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PathFileOpt {
+    #[clap(flatten)]
+    opt: PathSharedOpt,
+}
+impl PathFileOpt {
+    pub fn path(&self) -> Path {
+        self.opt.path()
+    }
+}
+
+impl ArgsDispatcher<Error> for PathFileOpt {
+    fn dispatch(&self) -> Result<()> {
+        let path = self.path();
+        println!("path: {path}");
+        Ok(())
+    }
+}

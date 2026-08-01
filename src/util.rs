43df00469a8e03a2f9e9c6ff4f48e22457da2901
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-01 14:50:37 +0000

    change ceiling of `discover_git_repo` to the filesystem root

--- src/util.rs
+++ src/util.rs
@@ -8,8 +8,9 @@ use crate::Result;
 pub fn discover_git_repo(starting_point: &Path) -> Result<(Repository, Path)>
 {
     let home = Path::new("~/").canonicalize()?.to_string();
-    let path = Repository::discover_path(starting_point, [home.as_str()])
-        .map(|pb| Path::from(pb))?;
+    let path =
+        Repository::discover_path(starting_point, [iocore::ROOT_PATH_STR])
+            .map(|pb| Path::from(pb))?;
     let repo = Repository::open::<Path>(path.clone())?;
     Ok((repo, path))
 }
